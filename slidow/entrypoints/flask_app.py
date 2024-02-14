1b6d4015436be93d2e4e0f0f2c370e04e8827083
Kenny Rachuonyo <krmboya@gmail.com>
2024-02-14 14:38:10 +0300

    add flask db session management

--- slidow/entrypoints/flask_app.py
+++ slidow/entrypoints/flask_app.py
@@ -1,18 +1,64 @@
-from flask import Blueprint, Flask
+import os
+
+from flask import Blueprint, Flask, current_app, g
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from slidow.adapters import repos
 
 slidow_bp = Blueprint("slidow", __name__)
 
 
 @slidow_bp.route("/", methods=("GET",))
-def landing_page():
+def root():
     return "<p> Welcome to slidow!</p>"
 
 
-def create_app(test_config=None):
-    app = Flask(__name__)
-    app.config.from_mapping(SECRET_KEY="dev")
-    if test_config:
-        app.config.from_mapping(test_config)
+@slidow_bp.route("/events", methods=("GET",))
+def events_list():
+    session = get_db_session()
+    events_repo = repos.EventSQLAlchemyRepo(session)
+    events = events_repo.list()
+    return " ".join([event.name for event in events])
 
+
+def get_db_session():
+    if "db_session" not in g:
+        Session = current_app.config["DB_SESSION_FACTORY"]
+        g.db_session = Session()
+    return g.db_session
+
+
+def close_db(e=None):
+    g.pop("db_session", None)
+    Session = current_app.config["DB_SESSION_FACTORY"]
+    Session.remove()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "slidow-dev.sqlite")
+    )
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        if db_name := test_config.get("db_name"):
+            app.config.from_mapping(DATABASE=os.path.join(app.instance_path, db_name))
+
+    # Create a scoped session factory for use in views
+    db_engine = create_engine("sqlite:///" + app.config["DATABASE"])
+    Session = scoped_session(sessionmaker(bind=db_engine))
+    app.config.from_mapping(DB_SESSION_FACTORY=Session)
+
+    # create instance folder in dev
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        ...
+
+    # register callbacks etc..
     app.register_blueprint(slidow_bp)
+    app.teardown_appcontext(close_db)
     return app
--- slidow/tests/e2e/test_flask_app.py
+++ slidow/tests/e2e/test_flask_app.py
@@ -1,13 +1,44 @@
 import unittest
 
+from sqlalchemy import text
+
+from slidow.adapters import orm
 from slidow.entrypoints.flask_app import create_app
 
 
 class FlaskAppTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app({"TESTING": True})
+
+        # db prep
+        self.app = create_app({"TESTING": True, "db_name": "slidow-test.sqlite"})
+        Session = self.app.config["DB_SESSION_FACTORY"]
+        bound_engine = Session.get_bind()
+        orm.mapper_registry.metadata.create_all(bound_engine)
+
         self.client = self.app.test_client()
+        self.session = Session()
+
+    def tearDown(self):
+
+        # db cleanup
+        self.session.close()
+        Session = self.app.config["DB_SESSION_FACTORY"]
+        bound_engine = Session.get_bind()
+        orm.mapper_registry.metadata.drop_all(bound_engine)
+        Session = self.app.config["DB_SESSION_FACTORY"]
+        Session.remove()
+
+    def insert_event(self, session, identifier, name) -> int:
+        session.execute(
+            text("insert into event (identifier, name)" " values (:identifier, :name)"),
+            dict(identifier=identifier, name=name),
+        )
+        (event_id,) = session.execute(
+            text("select id from event" " where identifier=:identifier"),
+            {"identifier": identifier},
+        )
+        return event_id
 
     def test_can_load_app(self):
         self.assertFalse(create_app().testing)
@@ -16,6 +47,17 @@ class FlaskAppTestCase(unittest.TestCase):
     def test_can_access_landing_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+
+    def test_can_see_events_list(self):
+        events = [("event1", "Event1"), ("event2", "Event2")]
+
+        for event in events:
+            self.insert_event(self.session, *event)
+
+        response = self.client.get("/events")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Event1", response.text)
+        self.assertIn("Event2", response.text)
 
 
 if __name__ == "__main__":
